29a05c54c5e0b3094522a53c35c6378d3fbc566f
kartikey Agarwal <kartikeyeagarwal1234@gmail.com>
2026-03-24 11:01:58 +0530

    Initial Commit

---
+++ TripMind/TripMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripMindApp.swift
+//  TripMind
+//
+//  Created by REAL  on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TripMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
